eb6ec6860f646303dcc08b443e1ab2c335b96a68
Jan Beinke <git@janbeinke.com>
2021-08-20 08:04:01 +0200

    Fix some of kibot funktinality

--- kibot.nix
+++ kibot.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, buildPythonPackage, fetchFromGitHub, pytest, pytest-cov, coverage, pyyaml, XlsxWriter, requests, colorama, kiauto, kicad, kibom, InteractiveHtmlBom, KiCost, PcbDraw, librsvg, imagemagick }:
+{ lib, pkgs, buildPythonPackage, fetchFromGitHub, pytest, pytest-cov, coverage, pyyaml, XlsxWriter, requests, colorama, kiauto, kibom, kicad-combined, InteractiveHtmlBom, KiCost, PcbDraw, librsvg, imagemagick }:
 
 buildPythonPackage rec {
   pname = "kibot";
@@ -15,20 +15,15 @@ buildPythonPackage rec {
     sed -i 's/COVERAGE_SCRIPT =.*/COVERAGE_SCRIPT = "coverage"/' tests/utils/context.py
   '';
 
-  buildInputs = [ ];
+  nativeBuildInputs = [ coverage ];
   buildPhase = ''
     python3 setup.py sdist bdist_wheel
     coverage run -a src/kibot --help-outputs > /dev/null
   '';
-  propagatedBuildInputs = [ pyyaml XlsxWriter requests colorama kiauto kicad kibom pkgs.zip pkgs.unzip pkgs.gnutar InteractiveHtmlBom KiCost pkgs.xlsx2csv PcbDraw librsvg imagemagick ];
+  propagatedBuildInputs = [ pyyaml XlsxWriter requests colorama kiauto kibom pkgs.zip pkgs.unzip pkgs.gnutar InteractiveHtmlBom KiCost pkgs.xlsx2csv PcbDraw librsvg imagemagick pkgs.libxslt kicad-combined ];
   checkInputs = [ pytest pytest-cov coverage ] ++ propagatedBuildInputs;
   # test_gerber_inner_ok uses the `rar` command wich is not available
   # test_kicad_conf_* tests may fail due to the way kicad works on nix
-  # kicosts tests are broken to unknown reasons
-  # pcbdraw tests are broken to unknown reasons
-  # pdf tests are broken to unknown reasons
-  # preflight (erc, drc, print, etc.) tests might be broken due to recordmydesktop being unsupported
-  # bom column tests are broken to unknown reasons
   checkPhase = ''
     export HOME=$(mktemp -d)
     mkdir $HOME/.config
@@ -40,38 +35,21 @@ buildPythonPackage rec {
     ' and not test_kicad_conf_local_conf'\
     ' and not test_kicost_simple'\
     ' and not test_kicost_bom_simple'\
+    ' and not test_makefile_1'\
+    ' and not test_makefile_2'\
+    ' and not test_pre_xrc_fail'\
+    ' and not test_step_fail'\
     ' and not test_pcbdraw_variant_1'\
     ' and not test_pcbdraw_variant_2'\
     ' and not test_pcbdraw_variant_3'\
     ' and not test_pdf_refill'\
     ' and not test_pdf_variant_1'\
-    ' and not test_erc_1'\
     ' and not test_erc_fail_1'\
     ' and not test_erc_warning_1'\
     ' and not test_erc_warning_2'\
-    ' and not test_drc_1'\
-    ' and not test_drc_filter'\
-    ' and not test_drc_unco'\
-    ' and not test_drc_error'\
-    ' and not test_update_xml'\
-    ' and not test_print_pcb_simple'\
-    ' and not test_print_pcb_refill'\
-    ' and not test_print_variant_1'\
-    ' and not test_print_pcb_options'\
-    ' and not test_print_wrong_paste'\
-    ' and not test_print_sch_ok'\
-    ' and not test_print_sch_svg_ok'\
     ' and not test_print_sch_variant_ni_1'\
     ' and not test_print_sch_svg_variant_ni_1'\
     ' and not test_print_sch_variant_ni_2'\
-    ' and not test_error_bom_column'\
-    ' and not test_makefile_1'\
-    ' and not test_makefile_2'\
-    ' and not test_pre_xrc_fail'\
-    ' and not test_step_fail'\
-    ' and not test_drc_time_out'\
-    ' and not test_sch_missing_1'\
-    ' and not test_sch_missing_filtered'\
     ' and not test_step_1'\
     ' and not test_step_3'\
     ' and not test_step_variant_1'
